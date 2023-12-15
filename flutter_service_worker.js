346d8bdd8613b601b219e058f226999629b85bdf
TatsuyaSagara <TatsuyaSagara@users.noreply.github.com>
2023-12-15 06:00:30 +0000

    deploy: f9ddaf6321e11488687240b655fc2cd672bfc277

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "manifest.json": "5a662627713f86a3c441bec36d415bca",
-"index.html": "74d6b2bdcf60515a6d040eb4821c1846",
-"/": "74d6b2bdcf60515a6d040eb4821c1846",
+"index.html": "cd00d70c01908de9171eced52c8119ae",
+"/": "cd00d70c01908de9171eced52c8119ae",
 "assets/AssetManifest.bin": "996b3a03c318650063eea29487613fed",
 "assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
 "assets/assets/images/1-icon.png": "879eb2265b9b7ea9f8ee93a1ba397001",
