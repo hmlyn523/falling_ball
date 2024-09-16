7f6d7d2028cf0d06671e4f4f16e4677f3ad38607
TatsuyaSagara <TatsuyaSagara@users.noreply.github.com>
2024-09-16 20:03:57 +0000

    deploy: 36639214c370161a795b3cc5b8bda777703da739

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"059e4e6d8ff6de39c29441c53e949bfb0bf179
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2990840353"
+    serviceWorkerVersion: "73869210"
   }
 });
