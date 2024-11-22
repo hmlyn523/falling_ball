78bbf0a244b9dccc13cc8d5f3b5b45486eac774d
TatsuyaSagara <TatsuyaSagara@users.noreply.github.com>
2024-11-22 16:39:03 +0000

    deploy: d51b96c91b5fece372183cc103b51ca18acb53b1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"main.dart.js": "7f3152c9b2965e07b7dc23966b1ccd44",
+"main.dart.js": "574bb5019d26edc98b668e2bbc0ca23e",
 "splash/img/light-3x.png": "ed5b2566ce8c5ba7c3424c36909dac66",
 "splash/img/light-1x.png": "0e11ccf013220e6454618376d6512300",
 "splash/img/light-2x.png": "997432f244fa18fc1b3bce575ef0968d",
@@ -73,7 +73,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "1d23ecbef36c5207710608f54dcdfee0",
-"flutter_bootstrap.js": "87bfcfaef368d5714fc094f0254c8bc8"};
+"flutter_bootstrap.js": "847c4601a3221171df9351c4f43f87cc"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
