820bf8e893361f77d18c51b59d615e08614234dd
TatsuyaSagara <tatsuya.sagara@icloud.com>
2025-01-12 12:45:16 +0900

    ✨ 落下アイテムを丸から四角に変更(不足分)

--- lib/features/game/components/fall_item/falling_item.dart
+++ lib/features/game/components/fall_item/falling_item.dart
@@ -1,3 +1,4 @@
+import 'package:falling_ball/features/game/components/wall.dart';
 import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
@@ -60,28 +61,44 @@ class FallingItem extends BodyComponent with ContactCallbacks {
 
   @override
   Body createBody() {
-    final shape = CircleShape();
-    shape.radius = radius;
+    final shape = PolygonShape();
+    shape.setAsBox(size.x / 2, size.y / 2, Vector2.zero(), 0.0); // 四角形の幅と高さを設定
     position.y += bump; // 位置調整
 
     final fixtureDef = FixtureDef(shape)
-      ..density = density // 密度
-      ..friction = 0.5 // 摩擦
-      ..restitution = 0.0 // 反発
+      ..density = density
+      ..friction = 0.5
+      ..restitution = 0.0
       ..filter.categoryBits = Config.CATEGORY_BALL
       ..filter.maskBits = Config.CATEGORY_BALL | Config.CATEGORY_DOWN_WALL | Config.CATEGORY_LEFT_WALL | Config.CATEGORY_RIGHT_WALL;
 
     final bodyDef = BodyDef()
-      ..userData = this // To be able to determine object in collision
+      ..userData = this
       ..position = this.position
       ..angle = (this.position.x + this.position.y) / 2 * 3.14
       ..angularDamping = 0.6
       ..type = BodyType.dynamic;
+
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 
   @override
   void beginContact(Object other, Contact contact) {
+    if (other is Wall) {
+      // FixtureA と FixtureB のカテゴリビットを取得
+      final fixtureA = contact.fixtureA;
+      final fixtureB = contact.fixtureB;
+
+      // 接触した Fixture が Wall に該当する場合、そのカテゴリビットを取得
+      final wallCategory = (fixtureA.body.userData == other)
+          ? fixtureA.filterData.categoryBits
+          : fixtureB.filterData.categoryBits;
+
+      // left または right の壁の場合は return
+      if (wallCategory == Config.CATEGORY_LEFT_WALL || wallCategory == Config.CATEGORY_RIGHT_WALL) {
+        return;
+      }
+    }
     _falling = false;
     contactCallback(this, other, contact);
   }
