c13d5802a0d167789472cffe316987f1604eedde
TatsuyaSagara <tatsuya.sagara@icloud.com>
2025-01-30 23:04:31 +0900

    🔧 自動落下位時間の変更(5秒→3秒)

--- lib/app/config.dart
+++ lib/app/config.dart
@@ -8,7 +8,7 @@ class Config {
   ];
 
   static const ENEMY_BALL_HEIGHT_INTERVAL = 1.0;
-  static const AUTO_FALLING_INTERVAL = 5;
+  static const AUTO_FALLING_INTERVAL = 3;
 
   // static const List<int> itemDischargeProbability = [15, 10, 10, 10, 10, 10, 10, 10, 10, 5];
   static const List<int> itemDischargeProbability = [22, 18, 20, 21, 19];
